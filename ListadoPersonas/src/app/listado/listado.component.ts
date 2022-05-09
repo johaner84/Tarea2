e519bd359c38b7b41f5fea35da83f4af30b05d52
Juan Rosario <jerosario@itla.edu.do>
2022-05-09 18:48:35 -0400

    Tarea2

---
+++ ListadoPersonas/src/app/listado/listado.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  Personas: string[] = ['Juan','Pedro','Maria','Elena','Mateo']
+
+  isDisabled: boolean = false
+
+}
